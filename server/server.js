27182e157699eee94a338111600b35ace0bb1088
Donovan Tong <donotong01@gmail.com>
2022-11-02 11:48:39 -0600

    Initial commit.

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+const { PORT } = proces.env
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
