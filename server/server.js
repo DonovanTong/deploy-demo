f574ca2e33f951bcfae9ee27b217f0bbfe50cabc
Donovan Tong <donotong01@gmail.com>
2022-11-02 12:19:29 -0600

    Second commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const path = require('path')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-const { PORT } = proces.env
+
+const { home } = require("./controllers/pageCtrl")
+
+app.get("/", home)
+
+const { PORT } = process.env
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
